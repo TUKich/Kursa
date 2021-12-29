2ddc7d307116e1ac905a28030d402246c57f2690
TUKich <TUK.eSports@yandex.ru>
2021-12-29 10:33:06 +0300

    курса

---
+++ Гот/Task2/Task2/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        public static float[] Hf;
+        public static float[] TempHf;
+        public static float[] Hf2;
+        public static float[] TempHf2;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("14.	Используя массивы, реализовать основные операции над множествами:\n" +
+    "-определение принадлежности элемента a множеству S;\n" +
+    "-добавление элемента а в S\n" 
+    +"-удаление элемента а из S\n"+
+    "-объединение элементов двух множеств.");
+            bool ready = true;
+            while (ready)
+            {
+                try
+                {
+                    if (Hf != null)
+                    Console.WriteLine("[{0}]", string.Join(", ", Hf));
+                    Console.WriteLine();
+                    Console.WriteLine("1.Вести множества\n" +
+                                      "2.Определение принадлежности элемента a множеству S\n" +
+                                      "3.Добавление элемента а в S\n" +
+                                      "4.Удаление элемента а из S\n" +
+                                      "5.Объединение элементов двух множеств\n" +
+                                      "6.Выход\n");
+                    int a = Convert.ToInt32(Console.ReadLine());
+
+
+                    switch (a)
+                    {
+                        case 1:
+                            {
+                                if (Hf == null)
+                                {
+                                    Hf = (float[])Split(Console.ReadLine()).Clone();
+                                }
+                                else
+                                {
+                                    Array.Clear(Hf, 0, Hf.Length);
+                                    Array.Resize(ref Hf, 0);
+                                    TempHf = (float[])Split(Console.ReadLine()).Clone();
+                                    Array.Resize(ref Hf, TempHf.Length);
+                                    Hf = (float[])TempHf.Clone();
+
+                                }
+
+                                break;
+                            }
+                        case 2:
+                            {
+                                if (Hf != null)
+                                {
+                                    Console.WriteLine("Ведите элемент:");
+                                    Console.WriteLine(isInArray(Hf, Convert.ToSingle(Console.ReadLine())));
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Массив пустой.");
+                                }
+                                break;
+                            }
+                        case 3:
+                            {
+                                if (Hf != null)
+                                {
+                                    Console.WriteLine("Индекс\n" +
+                                                      "Потом элемент");
+                                    Hf = addElement(Hf, Convert.ToInt32(Console.ReadLine()), Convert.ToSingle(Console.ReadLine()));
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Массив пустой.");
+                                }
+                                break;
+                            }
+                        case 4:
+                            {
+                                if (Hf != null)
+                                {
+                                    Console.WriteLine("Индекс");
+                                    Hf = removeElement(Hf, Convert.ToInt32(Console.ReadLine()));
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Массив пустой.");
+                                }
+                                break;
+                            }
+                        case 5:
+                            {
+                                if (Hf2 == null)
+                                {
+                                    Console.WriteLine("Ведите 2 массив");
+                                    Hf2 = (float[])Split(Console.ReadLine()).Clone();
+                                    Hf = mergeArrays(Hf, Hf2);
+                                }
+                                else
+                                {
+                                    Array.Clear(Hf2, 0, Hf2.Length);
+                                    Array.Resize(ref Hf2, 0);
+                                    Console.WriteLine("Ведите 2 массив");
+                                    TempHf2 = (float[])Split(Console.ReadLine()).Clone();
+                                    Array.Resize(ref Hf2, TempHf2.Length);
+                                    Hf2 = (float[])TempHf2.Clone();
+                                    Hf = mergeArrays(Hf, Hf2);
+                                }
+                                break;
+                            }
+                        case 6:
+                            {
+                                ready = false;
+                                break;
+                            }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        public static float[] Split(string number)
+        {
+            string[] words = number.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            float[] Sf = new float[words.Length];
+            int i = 0;
+            foreach (string s in words)
+            {
+                Sf[i] = Convert.ToSingle(s);
+                i++;
+            }
+
+            return Sf;
+        }
+        public static bool isInArray(float[] array, float element)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == element)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static float[] addElement(float[] array, int index, float element)
+        {
+            float[] result = new float[array.Length + 1];
+            for (int i = 0; i < index; i++)
+            {
+                result[i] = array[i];
+            }
+            result[index] = element;
+            for (int i = index + 1; i < result.Length; i++)
+            {
+                result[i] = array[i - 1];
+            }
+            return result;
+        }
+        public static float[] removeElement(float[] array, int index)
+        {
+            float[] result = new float[array.Length - 1];
+            int k = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (index != i)
+                {
+                    result[k] = array[i];
+                    k++;
+                }
+            }
+            return result;
+        }
+
+        public static float[] mergeArrays(float[] array1, float[] array2)
+        {
+            float[] result = new float[array1.Length + array2.Length];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                result[i] = array1[i];
+            }
+
+
+            for (int j = 0; j < array2.Length; j++)
+            {
+                result[array1.Length + j] = array2[j];
+
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Гот/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private double Max;
+        private double Min;
+        private double Pmin = 1;
+        private double Pmax = 1;
+        private double MaxX;
+        private double MinX;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.chart1.Series[0].Points.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.chart1.Series[0].Points.Clear();
+            try
+            {
+                
+                double a = Convert.ToDouble(this.textBoxA.Text), b = Convert.ToDouble(this.textBoxB.Text), h = Convert.ToDouble(this.textBoxH.Text);
+                if (h!=0) {
+                    List<double> results = new List<double>();
+                    List<double> xi = new List<double>();
+                    MaxX = 0;
+                    MinX = 0;
+                    Max = 0;
+                    Min = 0;
+                    Pmin = 1;
+                    Pmax = 1;
+                    for (double x = a; x <= b; x += h)
+                    {
+                        double divider = 1, result = 0;
+                        for (double k = 1; k <= 10; k++)
+                        {
+                            divider = Math.Sin(x);
+                            result += Math.Pow(-1, k) * (x * Math.Pow(divider, k)) / k;
+                        }
+                        this.chart1.Series[0].Points.AddXY(x, result);
+                        xi.Add(x);
+                        results.Add(result);
+                    }
+                    Array1(results, xi);
+                    for (double k = 1; k <= 6; k++)
+                    {
+                        Pmin *= (Math.Pow(MinX, k) / Factorial(k));
+                        Pmax *= (Math.Pow(MaxX, k) / Factorial(k));
+                    }
+                    this.label10.Text = "" + Pmin;
+                    this.label9.Text = "" + Pmax;
+                }
+                else {
+                    this.label6.Text = "h имеет нулевое значение";
+                }
+            }catch(Exception ex)
+            {
+                this.label6.Text = ex.Message;
+            }
+        }
+        private void Array1(List<double> t, List<double> ti)
+        {
+            double[] u = new double[t.Count];
+            for(int i =0; i<u.Length;i++)
+            {
+                u[i] = t.ElementAt(i);
+            }
+            Max = u.Max();
+            Min = u.Min();
+            t.Sort();
+            MaxX = ti[Array.IndexOf(u,Max)];
+            MinX = ti[Array.IndexOf(u,Min)];
+            this.label13.Text = "" + Min;
+            this.label14.Text = "" + Max;
+        }
+        private double Factorial(double n)
+        {
+            if (n == 1) return 1;
+            return n * Factorial(n - 1);
+        }
+
+    }
+}
